8745f3a2df496e04d447ba5fe69c583d7ce373cf
AbimbolaOO <abimbolaolayemiwhyte@gmail.com>
2020-11-23 00:38:02 -0800

    feat: completed repo seach drop down

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -2,6 +2,16 @@ import { repoNav, dropDown } from "./animation/index.mjs";
 import { renderData } from "./api/index.mjs";
 renderData();
 repoNav();
-dropDown("hide-header-icon-0", "hide-drop-down-0");
-dropDown("hide-header-icon-1", "hide-drop-down-1");
-dropDown("hide-header-icon-2", "hide-drop-down-2");
+dropDown("hide-header-icon-0", "hide-drop-down-0", "hide-drop-down");
+dropDown("hide-header-icon-1", "hide-drop-down-1", "hide-drop-down");
+dropDown("hide-header-icon-2", "hide-drop-down-2", "hide-drop-down");
+dropDown(
+  "search-bar-drop-down-btn-1",
+  "repo-search-bar-drop-down-1",
+  "hide-repo-search-bar-drop-down"
+);
+dropDown(
+  "search-bar-drop-down-btn-2",
+  "repo-search-bar-drop-down-2",
+  "hide-repo-search-bar-drop-down"
+);
